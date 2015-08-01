3ac4fe19e7646c2bcc0ab0416804f1418c6201df
glamix <shuhei.szk@gmail.com>
2015-08-01 17:29:26 +0000

    Finish user index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!, :except=>[:show]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  
   def show
     @user = User.find(params[:id])
   end
   
-    def create
+  def create
     @user = User.new(user_params)
     if @user.save
       sign_in @user
