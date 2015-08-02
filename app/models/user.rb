a8c026f4687f4bab33fa587ef612a7f15d120ce5
glamix <shuhei.szk@gmail.com>
2015-08-02 08:34:21 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates(:name, presence: true)
+  
+  
+  def feed
+    # 実装イメージ（未完成）
+    Micropost.where("user_id = ?", id)
+  end
 end
